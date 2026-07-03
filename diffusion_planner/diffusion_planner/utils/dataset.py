4956206106d098d1c7b44dfd41390e01e1eaef31
Max-Bin <vborisw@gmail.com>
2026-07-03 23:37:45 +0900

    fix: normalize unsigned npz metadata dtypes
    Signed-off-by: Max-Bin <vborisw@gmail.com>

--- diffusion_planner/diffusion_planner/utils/dataset.py
+++ diffusion_planner/diffusion_planner/utils/dataset.py
@@ -14,4 +14,7 @@ class DiffusionPlannerData(Dataset):
     def __getitem__(self, idx):
         data = np.load(self.data_list[idx], allow_pickle=True)
         data = dict(data)  # npz to dict
+        for key, value in data.items():
+            if isinstance(value, np.ndarray) and np.issubdtype(value.dtype, np.unsignedinteger) and value.dtype != np.uint8:
+                data[key] = value.astype(np.int64, copy=False)
         return data
