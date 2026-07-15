34014cf294393a12f30afc7441c1c3f341c683ad
Y.Hisaki <yhisaki31@gmail.com>
2026-07-15 19:23:06 +0900

    feat: add diffusion-independent turn indicator prediction network
    Signed-off-by: Y.Hisaki <yhisaki31@gmail.com>

--- diffusion_planner/diffusion_planner/grpo_epoch.py
+++ diffusion_planner/diffusion_planner/grpo_epoch.py
@@ -69,6 +69,7 @@ def _sft_step(raw_inputs, model, optimizer, args, ema, aug):
         args.alpha_neighbor_loss * loss["neighbor_prediction_loss"]
         + args.alpha_planning_loss * loss["ego_planning_loss"]
         + loss["turn_indicator_loss"]
+        + loss["independent_turn_indicator_loss"]
         + args.coeff_road_border_loss * loss["road_border_loss"]
         + args.coeff_neighbor_collision_loss * loss["neighbor_collision_loss"]
     )
